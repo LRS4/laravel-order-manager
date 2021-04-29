509fd5659298130af57cc581745be3c98dd0caf9
LRS4 <42714431+LRS4@users.noreply.github.com>
2021-04-29 15:53:30 +0100

    Add create route and skeleton view

---
+++ resources/views/pizzas/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <div class="flex justify-center">
+            <h1>Add a new pizza</h1>
+        </div> 
+    </div>
+</div>
+@endsection

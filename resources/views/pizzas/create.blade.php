8c54c578c9feb6e4047bd5151415c07f2f13cb47
LRS4 <42714431+LRS4@users.noreply.github.com>
2021-04-29 16:20:28 +0100

    Scaffold create order form

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -1,11 +1,37 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-        <div class="flex justify-center">
-            <h1>Add a new pizza</h1>
-        </div> 
-    </div>
+<div class="wrapper create-pizza">
+    <h1>Order a pizza</h1>
+    <form action="/pizzas" method="POST">
+        <label for="name">
+            Your name
+        </label>
+        <input type="text"
+            id="name" 
+            name="name" />
+
+        <label for="type">
+            Choose your pizza
+        </label>
+        <select name="type" id="type">
+            <option value="margarita">Margarita</option>
+            <option value="hawaiian">Hawaiian</option>
+            <option value="veg supreme">Veg Supreme</option>
+            <option value="volcano">Volcano</option>
+        </select>
+
+        <label for="type">
+            Choose your base
+        </label>
+        <select name="base" id="base">
+            <option value="cheese crust">Cheese crust</option>
+            <option value="garlic crust">Garlic crust</option>
+            <option value="thin & crispy">Thin & Crispy</option>
+            <option value="thick">Thick</option>
+        </select>
+
+        <input type="submit" value="Order pizza" />
+    </form>
 </div>
 @endsection

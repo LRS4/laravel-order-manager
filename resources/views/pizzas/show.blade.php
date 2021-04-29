96b7afada9e5ba18202fc92603a7e9bb95e9a1b7
LRS4 <42714431+LRS4@users.noreply.github.com>
2021-04-29 16:00:31 +0100

    Add database record for find one pizza to view

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -4,8 +4,13 @@
 <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
         <div class="flex justify-center">
-            <h1>Pizza List - {{ $id }}</h1>
+            <h1>Order for {{ $pizza->name }}</h1>
         </div> 
+        <div class="justify-center">
+            <p class="type">Type: {{ $pizza->type }}</p>
+            <p class="base">Type: {{ $pizza->base }}</p>
+        </div>
+        <a href="/pizzas" class="back"><-- Back to all</a>
     </div>
 </div>
 @endsection
